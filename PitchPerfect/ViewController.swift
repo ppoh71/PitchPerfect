e370e5b4786e8fcd54e4af66c2d51ae90f76b81a
Peter Pohlmann <ppoh71@gmail.com>
2018-08-28 14:04:02 +0200

    Buttons, Images, Layout 1. Controller, Outlets & Actions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var micButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    enum recordState{ case record, stop, pause}
+    var recordStartText: String = "Tap Microphone to Record";
+    var recordStopText: String = "Now Recording";
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.alpha = 0.1
+        stopButton.isEnabled = false
+        recordLabel.text = recordStartText
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        print("mic button pressed")
+        
+        recordingStatus(recordState.record)
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingStatus(recordState.stop)
+    }
+    
+    func recordingStatus(_ state: recordState ){
 
-
+        switch state {
+            case .record:
+                print("record")
+                stopButton.alpha = 1
+                stopButton.isEnabled = true
+                recordLabel.text = recordStopText
+                animateRecording()
+            case .stop:
+                stopButton.alpha = 0.1
+                stopButton.isEnabled = false
+                recordLabel.text = recordStartText
+                micButton.layer.removeAllAnimations()
+                micButton.alpha = 1
+            case .pause:
+            print("pause")
+        }
+    }
+    
+    func animateRecording(){
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.micButton.alpha = 0.4
+        })
+    }
 }
 
