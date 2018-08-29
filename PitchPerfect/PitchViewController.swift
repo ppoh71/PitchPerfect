9c8b45591f43f8a59e977110a09f802a59047bda
Peter Pohlmann <ppoh71@gmail.com>
2018-08-29 19:33:58 +0200

    sondfx buttons & audioEngine, attach&connect AudioNodes

--- PitchPerfect/PitchViewController.swift
+++ PitchPerfect/PitchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PitchViewController: UIViewController {
     @IBOutlet weak var fastButton: UIButton!
@@ -15,10 +16,16 @@ class PitchViewController: UIViewController {
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
     @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var fxLabel: UILabel!
     
-    var audioURL = "Test URL"
+    var audioURL:URL!
+    var player: AVAudioPlayer?
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
     
-    enum PlaybackAudio: Int{ case slow=0, fast, reverb, echo, chipmunk, vader}
+    enum SoundFx: Int{ case slow=0, fast, reverb, echo, chipmunk, vader}
     
     @IBAction func stopAction(_ sender: Any) {
         print("stop pressed")
@@ -28,19 +35,20 @@ class PitchViewController: UIViewController {
         print("fx pressed")
         print(sender.tag)
         
-        switch(PlaybackAudio(rawValue: sender.tag)!){
+        switch(SoundFx(rawValue: sender.tag)!){
         case .slow:
             print("play slow")
+            playAudio(rate: 0.5, pitch: nil, fx: .slow)
         case .fast:
-             print("play fast")
+            playAudio(rate: 1.5, pitch: nil, fx: .fast)
         case .reverb:
-             print("play reverb")
+            playAudio(rate: nil, pitch: nil, fx: .reverb)
         case .echo:
-             print("play echo")
+            playAudio(rate: nil, pitch: nil, fx: .echo)
         case .chipmunk:
-             print("play chip")
+            playAudio(rate: nil, pitch: 1000, fx: .chipmunk)
         case .vader:
-             print("play vader")
+            playAudio(rate: nil, pitch: -1000, fx: .vader)
         }
     }
     
@@ -51,16 +59,95 @@ class PitchViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         print("var on appear: \(audioURL)")
+        prepareAudio()
     }
+    
+    func prepareAudio(){
+        do {
+            audioFile = try AVAudioFile(forReading: audioURL as URL)
+            print("prepare audio success \(audioFile)")
+        } catch {
+            //let error = String(describing: error)
+            //fxLabel.text = error
+        }
+    }
+    
+    func playAudio(rate: Float?, pitch: Float?, fx: SoundFx){
+        audioEngine = AVAudioEngine()
+        audioPlayerNode = AVAudioPlayerNode()
+        audioPlayerNode.volume = 1
+        audioEngine.attach(audioPlayerNode)
+        
+        //get audiofile and set playback buffer
+        let audioFile = try! AVAudioFile(forReading: audioURL)
+        let buffer = AVAudioPCMBuffer(pcmFormat: audioFile.processingFormat, frameCapacity: AVAudioFrameCount(audioFile.length))
+        try! audioFile.read(into: buffer!, frameCount: AVAudioFrameCount(audioFile.length) )
+        
+        //attach and connect audioNodes for fx
+        switch fx {
+        case .slow, .fast, .chipmunk, .vader:
+            let pitchNode = AVAudioUnitTimePitch()
+            if let pitch = pitch{
+                pitchNode.pitch = pitch
+            }
+            if let rate = rate{
+                pitchNode.rate = rate
+            }
+            connectAudioNodes(fxNode: pitchNode, audioPlayerNode: audioPlayerNode, buffer: buffer)
+        case .reverb:
+            let reverbNode = AVAudioUnitReverb()
+            reverbNode.loadFactoryPreset(AVAudioUnitReverbPreset.cathedral)
+            reverbNode.wetDryMix = 50
+            connectAudioNodes(fxNode: reverbNode, audioPlayerNode: audioPlayerNode, buffer: buffer)
+        case .echo:
+            let echoNode = AVAudioUnitDistortion()
+            echoNode.loadFactoryPreset(.multiEcho1)
+            connectAudioNodes(fxNode: echoNode, audioPlayerNode: audioPlayerNode, buffer: buffer)
+        }
+        
+        
+        //assign audio effects to engine
+       // audioEngine.attach(pitchNode)
+       // audioEngine.attach(echoNode)
+       // audioEngine.connect(audioPlayerNode, to: echoNode, format: audioFile.processingFormat)
+       // audioEngine.connect(echoNode, to: audioEngine.mainMixerNode, format: buffer?.format)
+        audioPlayerNode.scheduleBuffer(buffer!, at: nil, options:[], completionHandler: nil)
+        //audioPlayerNode.scheduleBuffer(buffer!, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
+        
+        //if audioEngine.isRunning {
+        //    return
+        //}
+        
+        audioEngine.prepare()
 
+        do {
+            try audioEngine.start()
+        } catch {
+            print("nope start engine")
+            return
+        }
+
+         audioPlayerNode.play()
+    }
+    
+    @objc func stopAudio(){
+        
+    }
+    
+    func connectAudioNodes(fxNode: AVAudioNode,audioPlayerNode: AVAudioPlayerNode,  buffer: AVAudioPCMBuffer? ) {
+        audioEngine.attach(fxNode)
+        audioEngine.connect(audioPlayerNode, to: fxNode, format: audioFile.processingFormat)
+        audioEngine.connect(fxNode, to: audioEngine.mainMixerNode, format: buffer?.format)
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- PitchPerfect/RecordViewController.swift
+++ PitchPerfect/RecordViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 import AVFoundation
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBOutlet weak var micButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordLabel: UILabel!
     
-    var audioURL = "Sender AudioURL"
-    enum recordState{ case record, stop, pause}
+    var audioRecorder: AVAudioRecorder!
+    var recordingSession: AVAudioSession!
+
+    enum recordState{ case record, stop, pause, noPermit, failed}
     var recordStartText: String = "Tap Microphone to Record";
     var recordStopText: String = "Now Recording";
+    var recordNoPermitText: String = "You have to allow using the Mic";
+    var recordFailedText: String = "Recording failed, please try again";
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         stopButton.alpha = 0.1
         stopButton.isEnabled = false
         recordLabel.text = recordStartText
@@ -37,30 +40,84 @@ class RecordViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("mic button pressed")
         recordingStatus(recordState.record)
+        recordAudio()
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         recordingStatus(recordState.stop)
-        performSegue(withIdentifier: "PlaybackSegue", sender: audioURL)
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    func recordAudio(){
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedAudio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        let settings = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+                        AVEncoderBitRateKey: 16,
+                        AVNumberOfChannelsKey: 2,
+                        AVSampleRateKey: 44100.0] as [String : Any]
+       
+        let recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            try! audioRecorder = AVAudioRecorder(url: filePath!, settings: settings)
+            print("record 3")
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                       print("allowed to record")
+                        self.audioRecorder.isMeteringEnabled = true
+                        self.audioRecorder.delegate = self as AVAudioRecorderDelegate
+                        self.audioRecorder.prepareToRecord()
+                        self.audioRecorder.record()
+                        
+                    } else {
+                        // failed to record!
+                        print("failed 1")
+                        self.recordingStatus(recordState.noPermit)
+                    }
+                }
+            }
+        } catch {
+            print("failed 2")
+            recordingStatus(recordState.failed)
+        }
     }
     
     func recordingStatus(_ state: recordState ){
-
         switch state {
-            case .record:
-                print("record")
-                stopButton.alpha = 1
-                stopButton.isEnabled = true
-                recordLabel.text = recordStopText
-                animateRecording()
-            case .stop:
-                stopButton.alpha = 0.1
-                stopButton.isEnabled = false
-                recordLabel.text = recordStartText
-                micButton.layer.removeAllAnimations()
-                micButton.alpha = 1
-            case .pause:
-            print("pause")
+        case .record:
+            print("record")
+            stopButton.alpha = 1
+            stopButton.isEnabled = true
+            recordLabel.text = recordStopText
+            micButton.isEnabled = false
+            animateRecording()
+        case .stop:
+            stopButton.alpha = 0.1
+            stopButton.isEnabled = false
+            recordLabel.text = recordStartText
+            micButton.layer.removeAllAnimations()
+            micButton.alpha = 1
+            micButton.isEnabled = true
+        case .pause:
+             print("pause")
+        case .noPermit:
+            stopButton.alpha = 0.1
+            stopButton.isEnabled = false
+            recordLabel.text = recordNoPermitText
+            micButton.layer.removeAllAnimations()
+            micButton.alpha = 0.2
+        case .failed:
+            stopButton.alpha = 0.1
+            stopButton.isEnabled = false
+            recordLabel.text = recordFailedText
+            micButton.layer.removeAllAnimations()
+            micButton.alpha = 0.2
         }
     }
     
@@ -70,13 +127,21 @@ class RecordViewController: UIViewController {
         })
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "PlaybackSegue" {
-            let playbackVC = segue.destination as! PitchViewController
-            let audioURL = sender as! String
-            playbackVC.audioURL = audioURL
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("finish recording")
+        if flag {
+            performSegue(withIdentifier: "PlaybackSegue", sender: audioRecorder.url)
+        }else{
+            recordingStatus(.failed)
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PlaybackSegue" {
+            let playbackVC = segue.destination as! PitchViewController
+            let audioURL = sender as! URL
+            playbackVC.audioURL = audioURL
+        }
+    }
 }
 
