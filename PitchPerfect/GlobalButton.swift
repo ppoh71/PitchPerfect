f6c66aeaac47bbf8028c9fc9789cf35b1d800650
Peter Pohlmann <ppoh71@gmail.com>
2018-09-01 12:39:33 +0200

    globalbutton class for animations

--- PitchPerfect/GlobalButton.swift
+++ PitchPerfect/GlobalButton.swift
@@ -9,19 +9,57 @@
 import UIKit
 
 class GlobalButton: UIButton {
-        
-        override func draw(_ rect: CGRect) {
-            updateLayerProperties()
-            print("shadow button")
-        }
-        
-        func updateLayerProperties() {
-            self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75).cgColor
-            self.layer.shadowOffset = CGSize(width: 3, height: 3)
-            self.layer.shadowOpacity = 0.7
-            self.layer.shadowRadius = 6.0
-            self.layer.masksToBounds = false
-        }
-        
+    
+    override func draw(_ rect: CGRect) {
+        updateLayerProperties()
+        print("shadow button")
     }
+    
+    func updateLayerProperties() {
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75).cgColor
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.layer.shadowOpacity = 0.7
+        self.layer.shadowRadius = 6.0
+        self.layer.masksToBounds = false
+    }
+    
+    func fadeButton(fadeIn: Bool, useCompleteHandler: Bool){
+        let fadeInValue = fadeIn ? CGFloat(1) : CGFloat(0) //fade in when true, else fadeout by cgfloat value
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseIn, .curveEaseInOut, .allowUserInteraction], animations: {
+            self.alpha = CGFloat(fadeInValue)
+            self.transform = CGAffineTransform(scaleX: fadeInValue, y: fadeInValue)
+        }, completion: { (complete: Bool) in
+            if useCompleteHandler{
+                self.animateButton()
+            }
+        })
+    }
+    
+    func changeButtonImage(imageName: String, useCompleteHandler: Bool){
+        UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn, .curveEaseInOut, .allowUserInteraction], animations: {
+            self.alpha = 0
+            self.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
+        }, completion: { (complete: Bool) in
+            
+            if let image = UIImage(named: imageName) {
+                self.setImage(image, for: .normal)
+            }
+            self.fadeButton(fadeIn: true, useCompleteHandler: useCompleteHandler)
+            print("fade from button class")
+        })
+    }
+    
+    func animateButton(){
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: {
+            self.alpha = 0.3
+            self.transform = CGAffineTransform(scaleX: 0.775, y: 0.775)
+        })
+    }
+    
+    func removeAnimtion(){
+        self.layer.removeAllAnimations()
+    }
+    
+}
 
--- PitchPerfect/RecordViewController.swift
+++ PitchPerfect/RecordViewController.swift
@@ -11,16 +11,16 @@ import AVFoundation
 
 class RecordViewController: UIViewController, AVAudioRecorderDelegate {
 
-    @IBOutlet weak var micButton: UIButton!
-    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var micButton: GlobalButton!
     @IBOutlet weak var recordLabel: UILabel!
     
     var audioRecorder: AVAudioRecorder!
     var recordingSession: AVAudioSession!
 
-    enum recordState{ case record, stop, pause, noPermit, failed}
-    var recordStartText: String = "Tap Microphone to Record";
-    var recordStopText: String = "Now Recording";
+    enum RecordState{ case record, stop, pause, noPermit, failed}
+    enum AimationComplete: Int{ case record = 0, fadeIn, none}
+    var recordStartText: String = "press mic to record";
+    var recordStopText: String = " .... recording ....";
     var recordNoPermitText: String = "You have to allow using the Mic";
     var recordFailedText: String = "Recording failed, please try again";
     
@@ -28,27 +28,28 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        stopButton.alpha = 0.1
-        stopButton.isEnabled = false
-        recordLabel.text = recordStartText
-        self.navigationItem.setHidesBackButton(true, animated:true);
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        
+        setupUI()
     }
     
-    @IBAction func recordAudio(_ sender: Any) {
+    @IBAction func recordAudio(_ sender: UIButton) {
         print("mic button pressed")
-        recordingStatus(recordState.record)
-        recordAudio()
-    }
-    
-    @IBAction func stopRecording(_ sender: Any) {
-        recordingStatus(recordState.stop)
-        audioRecorder.stop()
-        let audioSession = AVAudioSession.sharedInstance()
-        try! audioSession.setActive(false)
+       sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected{
+            recordingStatus(RecordState.record)
+            recordAudio()
+        }else{
+            recordingStatus(RecordState.stop)
+            audioRecorder.stop()
+            let audioSession = AVAudioSession.sharedInstance()
+            try! audioSession.setActive(false)
+        }
     }
     
     func recordAudio(){
@@ -67,7 +68,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
             try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
             try recordingSession.setActive(true)
             try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
-            print("record 3")
+
             recordingSession.requestRecordPermission() { [unowned self] allowed in
                 DispatchQueue.main.async {
                     if allowed {
@@ -79,54 +80,80 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
                         
                     } else {
                         // failed to record!
-                        print("failed 1")
-                        self.recordingStatus(recordState.noPermit)
+                        self.recordingStatus(RecordState.noPermit)
                     }
                 }
             }
         } catch {
-            print("failed 2")
-            recordingStatus(recordState.failed)
+            recordingStatus(RecordState.failed)
         }
     }
     
-    func recordingStatus(_ state: recordState ){
+    func recordingStatus(_ state: RecordState ){
         switch state {
         case .record:
-            print("record")
-            stopButton.alpha = 1
-            stopButton.isEnabled = true
             recordLabel.text = recordStopText
-            micButton.isEnabled = false
-            animateRecording()
+            micButton.changeButtonImage(imageName: "mic-stop", useCompleteHandler: true)
         case .stop:
-            stopButton.alpha = 0.1
-            stopButton.isEnabled = false
-            recordLabel.text = recordStartText
-            micButton.layer.removeAllAnimations()
-            micButton.alpha = 1
-            micButton.isEnabled = true
+            micButton.fadeButton(fadeIn: false, useCompleteHandler: false)
+            //nothing to do much
+            // break
         case .pause:
              print("pause")
         case .noPermit:
-            stopButton.alpha = 0.1
-            stopButton.isEnabled = false
             recordLabel.text = recordNoPermitText
             micButton.layer.removeAllAnimations()
             micButton.alpha = 0.2
         case .failed:
-            stopButton.alpha = 0.1
-            stopButton.isEnabled = false
             recordLabel.text = recordFailedText
             micButton.layer.removeAllAnimations()
             micButton.alpha = 0.2
         }
     }
     
-    func animateRecording(){
-        UIView.animate(withDuration: 0.4, delay: 0, options: [.repeat, .autoreverse], animations: {
-            self.micButton.alpha = 0.4
+    func fadeOut(){
+        
+        /*
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseIn, .curveEaseInOut, .allowUserInteraction], animations: {
+            self.micButton.alpha = 0
+            self.micButton.transform = CGAffineTransform(scaleX: 0.3, y: 0.3)
+        }, completion: { (complete: Bool) in
+            if let image = UIImage(named: "mic-stop") {
+                self.micButton.setImage(image, for: .normal)
+            }
+            self.fadeIn()
         })
+        */
+    }
+    
+    func fadeIn(){
+         /*
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseIn, .curveEaseInOut, .allowUserInteraction], animations: {
+            self.micButton.alpha = 1
+            self.micButton.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }, completion: { (complete: Bool) in
+            self.animateRecording()
+        })
+         */
+    }
+    
+    func animateRecording(){
+        /*
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: {
+            self.micButton.alpha = 0.3
+            self.micButton.transform = CGAffineTransform(scaleX: 0.775, y: 0.775)
+        }, completion: { (complete: Bool) in
+            print("complete")
+        })
+        */
+    }
+    
+    func setupUI(){
+        recordLabel.text = recordStartText
+        micButton.alpha = 0
+        micButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        micButton.changeButtonImage(imageName: "microphone2x-iphone", useCompleteHandler: false)
+                
     }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
